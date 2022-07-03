3310600a9eda4f5d083b5a79d2388725e2dfbc55
= <tmelo387@gmail.com>
2022-07-03 20:04:40 -0300

    Project Created

---
+++ src/main/java/com/crudlvh/crudlvch/CrudLvcHApplication.java
@@ -0,0 +1,13 @@
+package com.crudlvh.crudlvch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudLvcHApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudLvcHApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crudlvh/crudlvch/CrudLvcHApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudlvh.crudlvch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudLvcHApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

331e1d3a65f42c15a150b8b5204faab4427078eb
= <tmelo387@gmail.com>
2022-07-05 18:19:43 -0300

    fix(Import): Unused imports

--- src/main/java/com/crudlvh/crudlvch/controller/RegistroCasoLVC.java
+++ src/main/java/com/crudlvh/crudlvch/controller/RegistroCasoLVC.java
@@ -3,7 +3,6 @@ package com.crudlvh.crudlvch.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.hibernate.loader.collection.PaddedBatchingCollectionInitializerBuilder;
 
 // import java.time.LocalDate;
 // import java.util.List;
